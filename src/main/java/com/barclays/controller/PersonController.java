259baef4ec9f074a651b156e2ce8fb4845a20719
shonanohs <shonafitzpatrick15@gmail.com>
2023-11-13 13:40:21 +0000

    Added controller/mapping annotations

---
+++ src/main/java/com/barclays/controller/PersonController.java
@@ -0,0 +1,21 @@
+package com.barclays.controller;
+
+import com.barclays.model.Person;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+public class PersonController {
+
+    @GetMapping("/people")
+    public List<Person> getPeople() {
+        List<Person> people = new ArrayList<>();
+        Person person = new Person();
+        person.setName("Shona");
+        people.add(person);
+        return people;
+    }
+}
